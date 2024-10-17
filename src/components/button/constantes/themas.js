5aa748dad1ff239f4fe75086e1327509ec005dc6
Fabio Macieira <fabiomacieira16@gmail.com>
2024-10-16 22:03:06 -0300

    commit inital project

---
+++ src/components/button/constantes/themas.js
@@ -0,0 +1,30 @@
+export const COLORS = {
+    primary: '#0D6EFD',
+    secondary: '#6C757D',
+    success: '#198754',
+    danger: '#DC3545',
+    warning: '#FFC107',
+    info: '#0DCAF0',
+    light: '#F8F9FA',
+    dark: '#212529',
+    white: '#FFFFFF',
+    black: '#000000',
+};
+
+export const SIZES = {
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 40,
+};
+
+export const FONTS = {
+    ssm: 12,
+    sm: 14, 
+    md: 16,
+    lg: 18,
+    xl: 32,
+    xxl: 64,
+};
+
---
+++ src/screens/abahome/abahome.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { styles } from "./abahome.style";
+
+
+function AbaHome() {
+  return (
+    <View style={styles.container}> 
+      <Text style={styles.text}>Agende os seus serviços médico</Text>
+    </View>
+
+  );
+}
+
+export default AbaHome;
+
+  
